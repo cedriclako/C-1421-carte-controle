76e4115dae33e7d4b910b92b1bd4a137231cc2f2
Maxime Carrier <mcarrier@novika.ca>
2022-12-14 12:47:45 -0500

    UART decoder ...

--- ESP32-Remote-Prototype/esp32-components/uart-protocol/uart-protocol.c
+++ ESP32-Remote-Prototype/esp32-components/uart-protocol/uart-protocol.c
@@ -1,7 +1,119 @@
 #include <stdio.h>
+#include <assert.h>
 #include "uart-protocol.h"
 
-void func(void)
-{
+static void AddByte(UARTPROTOCOL_SHandle* psHandle, uint8_t u8);
 
+static void DropFrame(UARTPROTOCOL_SHandle* psHandle, const char* szReason);
+static void AcceptFrame(UARTPROTOCOL_SHandle* psHandle);
+
+void UARTPROTOCOL_Init(UARTPROTOCOL_SHandle* psHandle, const UARTPROTOCOL_SConfig* psConfig)
+{
+    assert(psHandle != NULL && psConfig != NULL && psConfig->u8PayloadBuffers != NULL);
+    psHandle->psConfig = psConfig;
+}
+
+void UARTPROTOCOL_Reset(UARTPROTOCOL_SHandle* psHandle)
+{
+    psHandle->eStep = UARTPROTOCOL_ESTEP_WaitingForStartByte;
+    psHandle->u32PayloadCount = 0;
+
+    psHandle->u8FrameID = 0;
+
+    psHandle->u8ChecksumCalculation = 0;
+}
+
+void UARTPROTOCOL_HandleIn(UARTPROTOCOL_SHandle* psHandle, const uint8_t* u8Datas, uint32_t u32DataLen)
+{
+    for(uint32_t i = 0; i < u32DataLen; i++)
+        AddByte(psHandle, u8Datas[i]);
+}
+
+static void AddByte(UARTPROTOCOL_SHandle* psHandle, uint8_t u8)
+{
+    switch(psHandle->eStep)
+    {
+        case UARTPROTOCOL_ESTEP_WaitingForStartByte:
+        {
+            // Wait until we get a start byte ...
+            if (u8 == UARTPROTOCOL_START_BYTE)
+            {
+                psHandle->u8ChecksumCalculation = 0;
+                psHandle->eStep = UARTPROTOCOL_ESTEP_WaitingFrameID;
+            }
+            break;
+        }
+        case UARTPROTOCOL_ESTEP_WaitingFrameID:
+        {
+            psHandle->u8FrameID = u8;
+            psHandle->eStep = UARTPROTOCOL_ESTEP_WaitingPayloadLength;
+            psHandle->u8ChecksumCalculation += u8;
+            break;
+        }
+        case UARTPROTOCOL_ESTEP_WaitingPayloadLength:
+        {
+            psHandle->u8FramePayloadLen = u8;
+            psHandle->u8ChecksumCalculation += u8;
+            psHandle->eStep = UARTPROTOCOL_ESTEP_GettingPayload;
+            break;
+        }
+        case UARTPROTOCOL_ESTEP_GettingPayload:
+        {
+            if (psHandle->u32PayloadCount + 1 >= (uint32_t)psHandle->psConfig->u8PayloadBufferLen)
+            {
+                DropFrame(psHandle, "Payload is too big for the buffer");
+                break;
+            }
+
+            psHandle->psConfig->u8PayloadBuffers[psHandle->u32PayloadCount] = u8;
+            psHandle->u8ChecksumCalculation += u8;
+            psHandle->u32PayloadCount++;
+
+            // Complete payload detected ...
+            if (psHandle->u32PayloadCount >= psHandle->u8FramePayloadLen)
+                psHandle->eStep = UARTPROTOCOL_ESTEP_WaitingChecksum;
+            break;
+        }
+        case UARTPROTOCOL_ESTEP_WaitingChecksum:
+        {
+            // Bitwise operation on calculated checksum ...
+            // Checksum arrived ...
+            if (~psHandle->u8ChecksumCalculation != u8)
+            {
+                DropFrame(psHandle, "Invalid checksum");
+                break;
+            }
+            
+            psHandle->eStep = UARTPROTOCOL_ESTEP_WaitingStopByte;
+            break;
+        }
+        case UARTPROTOCOL_ESTEP_WaitingStopByte:
+        {
+            // If we reach this point, the checksum passed
+            if (u8 != UARTPROTOCOL_STOP_BYTE)
+            {
+                DropFrame(psHandle, "Stop byte");
+                break;
+            }
+
+            // If we reach this point it's good.
+            AcceptFrame(psHandle);
+            UARTPROTOCOL_Reset(psHandle);
+            break;
+        }
+    }
+}
+
+static void AcceptFrame(UARTPROTOCOL_SHandle* psHandle)
+{
+    if (psHandle->psConfig->fnAcceptFrameCb != NULL)
+        psHandle->psConfig->fnAcceptFrameCb(psHandle, psHandle->psConfig->u8PayloadBuffers, (uint8_t)psHandle->u32PayloadCount);
+    UARTPROTOCOL_Reset(psHandle);
+}
+
+static void DropFrame(UARTPROTOCOL_SHandle* psHandle, const char* szReason)
+{
+    if (psHandle->psConfig->fnDropFrameCb != NULL)
+        psHandle->psConfig->fnDropFrameCb(psHandle, szReason);
+    UARTPROTOCOL_Reset(psHandle);
 }
---
+++ ESP32-Remote-Prototype/esp32-components/uart-protocol/uart-protocol.h
@@ -0,0 +1,60 @@
+#ifndef _UART_PROTOCOL_H_
+#define _UART_PROTOCOL_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define UARTPROTOCOL_MAXPAYLOAD 255
+#define UARTPROTOCOL_START_BYTE 0xCC
+#define UARTPROTOCOL_STOP_BYTE 0x99
+
+// Looks like this: [START BYTE] [Frame ID] [PAYLOAD LENGTH] [PAYLOAD  ...] [CHECKSUM] [STOP BYTE] 
+typedef enum
+{
+  UARTPROTOCOL_ESTEP_WaitingForStartByte,  
+  UARTPROTOCOL_ESTEP_WaitingFrameID,  
+  UARTPROTOCOL_ESTEP_WaitingPayloadLength,
+  UARTPROTOCOL_ESTEP_GettingPayload,
+  UARTPROTOCOL_ESTEP_WaitingChecksum,
+  UARTPROTOCOL_ESTEP_WaitingStopByte
+} UARTPROTOCOL_ESTEP;
+
+typedef struct _UARTPROTOCOL_SHandle UARTPROTOCOL_SHandle;
+
+typedef void (*FnAcceptFrame)(const UARTPROTOCOL_SHandle* psHandle, const uint8_t u8Payloads[], uint8_t u8PayloadLen);
+typedef void (*FnDropFrame)(const UARTPROTOCOL_SHandle* psHandle, const char* szReason);
+
+typedef struct 
+{
+    uint8_t* u8PayloadBuffers;
+    uint8_t u8PayloadBufferLen;
+
+    // Callback configs
+    const FnAcceptFrame fnAcceptFrameCb;
+    const FnDropFrame fnDropFrameCb;
+} UARTPROTOCOL_SConfig;
+
+struct _UARTPROTOCOL_SHandle
+{
+    // Current step
+    UARTPROTOCOL_ESTEP eStep;
+    uint32_t u32PayloadCount;
+
+    // Frame payload len. 
+    uint8_t u8FrameID;
+    uint8_t u8FramePayloadLen;
+
+    // On the fly checksum calculation
+    uint8_t u8ChecksumCalculation;
+
+    const UARTPROTOCOL_SConfig* psConfig;
+};
+
+void UARTPROTOCOL_Init(UARTPROTOCOL_SHandle* psHandle, const UARTPROTOCOL_SConfig* psConfig);
+
+void UARTPROTOCOL_Reset(UARTPROTOCOL_SHandle* psHandle);
+
+void UARTPROTOCOL_HandleIn(UARTPROTOCOL_SHandle* psHandle, const uint8_t* u8Datas, uint32_t u32DataLen);
+
+#endif
