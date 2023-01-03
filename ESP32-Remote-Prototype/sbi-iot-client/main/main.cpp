99681bcf7136a2f125a540bcf4cd9a8d4e481b87
Maxime Carrier <mcarrier@novika.ca>
2023-01-03 14:41:55 -0500

    repair launch file

--- ESP32-Remote-Prototype/sbi-iot-client/main/main.cpp
+++ ESP32-Remote-Prototype/sbi-iot-client/main/main.cpp
@@ -19,24 +19,6 @@
 #define SCREEN_WIDTH 540
 #define SCREEN_HEIGHT 960
 
-typedef struct 
-{
-    struct
-    {
-        uint32_t u32WakeUpCount;
-        uint8_t u8ESPNowChannel;
-
-        uint8_t u8NoDataCount;
-    } sData;
-    uint8_t u8Checksum;
-} SDeepSleepState;
-
-static RTC_DATA_ATTR SDeepSleepState m_sDeepSleepState;
-
-static uint8_t CalculateDeepSleepDataChecksum(const SDeepSleepState* pDeepSleepState);
-static void UpdateDeepSleepDataChecksum(SDeepSleepState* pDeepSleepState);
-static bool CheckDeepSleepDataChecksum(SDeepSleepState* pDeepSleepState);
-
 static void ENS2CGetStatusRespCallback(const SBI_iot_S2CGetStatusResp* pMsg);
 static void ENChannelFoundCallback(uint8_t u8Channel);
 
@@ -45,27 +27,16 @@ static void DrawAllBars(int32_t s32X, int32_t s32Y, uint32_t u32Bar);
 static void UpdateScreen();
 
 static bool m_bDataReceived = false;
+static bool m_isUserModeActive = false;
+
+static bool m_bIsNeedClear = true;
+
 static TickType_t m_ttProcTimeoutTicks = xTaskGetTickCount();
 
 static M5EPD_Canvas m_CanvasResult(&M5.EPD);
 
 void setup()
 {
-    // Check if RTC memory is already initialized.
-    if (!CheckDeepSleepDataChecksum(&m_sDeepSleepState))
-    {
-        ESP_LOGI(TAG, "Init deep sleep data conservation");
-        // Initialize deep sleep data structure
-        memset(&m_sDeepSleepState.sData, 0, sizeof(m_sDeepSleepState.sData));
-    }
-
-    m_sDeepSleepState.sData.u32WakeUpCount++;
-    UpdateDeepSleepDataChecksum(&m_sDeepSleepState);
-    ESP_LOGI(TAG, "Booted: %d ms, boot count: %d, esp-now channel: %d", 
-        (int)(esp_timer_get_time() / 1000), 
-        m_sDeepSleepState.sData.u32WakeUpCount,
-        m_sDeepSleepState.sData.u8ESPNowChannel);
-
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -87,15 +58,16 @@ void setup()
     ESP_LOGI(TAG, "CreateCanvas");
     m_CanvasResult.createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-
     M5.update();
     if (M5.BtnP.isPressed())
     {
+        m_isUserModeActive = true;
+
         ESP_LOGI(TAG, "Started using the button");
         M5.EPD.Clear(true);
         m_CanvasResult.setTextSize(2);
         m_CanvasResult.setFreeFont(FF19);
-        m_CanvasResult.drawString("Powering on ...", 0, 850);
+        m_CanvasResult.drawCentreString("Powering on ...", SCREEN_WIDTH/2, 850, GFXFF);
         m_CanvasResult.pushCanvas(0, 0, UPDATE_MODE_A2);
         m_CanvasResult.fillCanvas(0);
     }
@@ -105,7 +77,7 @@ void setup()
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     // Restore last channel
-    ESPNOWCOMM_Init(m_sDeepSleepState.sData.u8ESPNowChannel);
+    ESPNOWCOMM_Init(0);
 
     ESPNOWCOMM_SetChannelFoundCallback(ENChannelFoundCallback);
     ESPNOWCOMM_SetS2CGetStatusRespCallback(ENS2CGetStatusRespCallback);
@@ -114,27 +86,55 @@ void setup()
 }
 
 void loop()
-{
+{ 
+    static bool bIsNeedUpdate = true;
+
     ESPNOWCOMM_Handler();
     
     M5.SHT30.UpdateData();
 
-    // 5s maximum, after that we go to sleep again.
-    if ((xTaskGetTickCount() - m_ttProcTimeoutTicks) > pdMS_TO_TICKS(10000) || m_bDataReceived)
+    if (m_isUserModeActive)
+    {
+        if (bIsNeedUpdate)
+        {
+            bIsNeedUpdate = false;
+            // Update screen
+            UpdateScreen();
+        }
+
+        if (M5.TP.available()) 
+        {
+            if (!M5.TP.isFingerUp()) 
+            {
+                M5.TP.update();
+                tp_finger_t sFinger = M5.TP.readFinger(1);
+
+                ESP_LOGI(TAG, "finger, x: %d, y: %d", (int)sFinger.x, (int)sFinger.y);
+                
+                m_ttProcTimeoutTicks = xTaskGetTickCount();
+                bIsNeedUpdate = true;
+            }
+        }
+    }
+
+
+    // 10s maximum, after that we go to sleep again
+    bool bIsExpired = (!m_isUserModeActive && ( (xTaskGetTickCount() - m_ttProcTimeoutTicks) > pdMS_TO_TICKS(10*1000) || m_bDataReceived )) ||
+                      (m_isUserModeActive && (xTaskGetTickCount() - m_ttProcTimeoutTicks) > pdMS_TO_TICKS(60*1000));
+
+    if (bIsExpired)
     {
         // Timeout
         if (!m_bDataReceived)
         {
             ESP_LOGE(TAG, "No communication, reset ESPNowChannel, next time it will scan");
-            m_sDeepSleepState.sData.u8ESPNowChannel = 0;
-            UpdateDeepSleepDataChecksum(&m_sDeepSleepState);
         }
 
         // Update screen
+        m_isUserModeActive = false;
         UpdateScreen();
 
-        ESP_LOGI(TAG, "Time to go to sleep, good night. wk.count: %d, time: %d", 
-            m_sDeepSleepState.sData.u32WakeUpCount, 
+        ESP_LOGI(TAG, "Time to go to sleep, good night. time: %d", 
             (int)(esp_timer_get_time() / 1000));
         
         // Shutdown make deep sleep useless, except when the USB port keep the device alive
@@ -149,15 +149,16 @@ void loop()
             vTaskDelay(1);
         }
         esp_restart();
-        //esp_sleep_enable_timer_wakeup(10 * 1000000);
-        //esp_deep_sleep_start();
     }
 }
 
 static void UpdateScreen()
 {
-    ESP_LOGI(TAG, "Clear EPD");
-    M5.EPD.Clear(true);
+    if (m_bIsNeedClear) 
+    {
+        ESP_LOGI(TAG, "Clear EPD");
+        M5.EPD.Clear(true);
+    }
     
     // rtc_time_t RTCtime;
     // rtc_date_t RTCDate;
@@ -203,9 +204,11 @@ static void UpdateScreen()
         m_CanvasResult.setFreeFont(FF19);
         m_CanvasResult.setTextSize(1);
         m_CanvasResult.drawCentreString("Setpoint", 40+120/2, s32CurrentTempY+92, GFXFF);
-        m_CanvasResult.drawJpg(pSFileArrowUp->pu8StartAddr, pSFileArrowUp->u32Length, 40, s32CurrentTempY+16);
-        m_CanvasResult.drawJpg(pSFileArrowDown->pu8StartAddr, pSFileArrowDown->u32Length, 40, s32CurrentTempY+136);
-
+        if (m_isUserModeActive)
+        {
+            m_CanvasResult.drawJpg(pSFileArrowUp->pu8StartAddr, pSFileArrowUp->u32Length, 40, s32CurrentTempY+16);
+            m_CanvasResult.drawJpg(pSFileArrowDown->pu8StartAddr, pSFileArrowDown->u32Length, 40, s32CurrentTempY+136);
+        }
         m_CanvasResult.setFreeFont(FSSB18);
         m_CanvasResult.setTextSize(3);
         m_CanvasResult.drawString(String(temStr), 232, s32CurrentTempY+72);
@@ -223,18 +226,30 @@ static void UpdateScreen()
         m_CanvasResult.setFreeFont(FF19);
         m_CanvasResult.setTextSize(1);
         m_CanvasResult.drawCentreString("Fan speed", 40+120/2, s32CurrentTempY+92, GFXFF);
-        m_CanvasResult.drawJpg(pSFileArrowUp->pu8StartAddr, pSFileArrowUp->u32Length, 40, s32CurrentTempY+16);
-        m_CanvasResult.drawJpg(pSFileArrowDown->pu8StartAddr, pSFileArrowDown->u32Length, 40, s32CurrentTempY+136);
-
+        if (m_isUserModeActive)
+        {
+            m_CanvasResult.drawJpg(pSFileArrowUp->pu8StartAddr, pSFileArrowUp->u32Length, 40, s32CurrentTempY+16);
+            m_CanvasResult.drawJpg(pSFileArrowDown->pu8StartAddr, pSFileArrowDown->u32Length, 40, s32CurrentTempY+136);
+        }
         DrawAllBars(232, s32CurrentTempY, 3);
 
         m_CanvasResult.drawRect(40, s32CurrentTempY + 216, SCREEN_WIDTH-(40*2), 2, TFT_WHITE);
     }
 
-    m_CanvasResult.drawJpg(pSFileSetting->pu8StartAddr, pSFileSetting->u32Length, 344, 760);
+    if (m_isUserModeActive)
+    {
+        m_CanvasResult.drawJpg(pSFileSetting->pu8StartAddr, pSFileSetting->u32Length, 344, 760);
+    }
+
     m_CanvasResult.drawJpg(pSFileSBILogo->pu8StartAddr, pSFileSBILogo->u32Length, 16, 832);
 
-    m_CanvasResult.pushCanvas(0, 0, UPDATE_MODE_DU);
+    // Update screen
+    if (m_bIsNeedClear)
+        m_CanvasResult.pushCanvas(0, 0, UPDATE_MODE_DU);
+    else
+        m_CanvasResult.pushCanvas(0, 0, UPDATE_MODE_DU4);
+
+    m_bIsNeedClear = false;
     vTaskDelay(pdMS_TO_TICKS(300));
 }
 
@@ -272,9 +287,6 @@ extern "C" void app_main()
 static void ENChannelFoundCallback(uint8_t u8Channel)
 {
     // Keep last known channel
-    m_sDeepSleepState.sData.u8ESPNowChannel = u8Channel;
-    UpdateDeepSleepDataChecksum(&m_sDeepSleepState);
-
     ESP_LOGI(TAG, "Right channel found!, channel: %d", u8Channel);
 }
 
@@ -295,28 +307,4 @@ static void ENS2CGetStatusRespCallback(const SBI_iot_S2CGetStatusResp* pMsg)
         ESP_LOGE(TAG, "GetStatus received but no stove_state");
     }
    m_bDataReceived = true;
-}
-
-static uint8_t CalculateDeepSleepDataChecksum(const SDeepSleepState* pDeepSleepState)
-{
-    uint8_t u8Checksum = 0;
-    for(int i = 0; i < sizeof(pDeepSleepState->sData); i++)
-    {
-        const uint8_t u8 = *((uint8_t*)&pDeepSleepState->sData + i);
-        u8Checksum += u8;
-    }
-    return ~u8Checksum;
-}
-
-static void UpdateDeepSleepDataChecksum(SDeepSleepState* pDeepSleepState)
-{
-    pDeepSleepState->u8Checksum = CalculateDeepSleepDataChecksum(pDeepSleepState);
-}
-
-static bool CheckDeepSleepDataChecksum(SDeepSleepState* pDeepSleepState)
-{
-    const uint8_t u8Checksum  = CalculateDeepSleepDataChecksum(pDeepSleepState);
-
-    ESP_LOGI(TAG, "Curr checksum: %d, last checksum: %d", u8Checksum, pDeepSleepState->u8Checksum);
-    return u8Checksum == pDeepSleepState->u8Checksum;
 }
