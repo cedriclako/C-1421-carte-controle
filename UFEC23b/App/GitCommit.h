a61d4f92b4ac9f3c9c9190670200462ec35dec0e
Maxime Carrier <mcarrier@novika.ca>
2023-09-26 09:26:04 -0400

    rename

---
+++ UFEC23b/App/GitCommit.h
@@ -0,0 +1,9 @@
+﻿#ifndef _GITCOMMIT_ID_H_
+#define _GITCOMMIT_ID_H_
+
+#define GITCOMMIT_COMMITID "feature/mcarrier/sbiserver-idf-5.1"
+#define GITCOMMIT_BRANCH "3995ee4"
+#define GITCOMMIT_ISDIRTY 1
+
+#endif
+
